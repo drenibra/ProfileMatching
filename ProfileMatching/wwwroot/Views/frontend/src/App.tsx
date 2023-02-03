14de75b7cf73aac296cb728c7a4299acf9c2cb66
drenibra <drenibrahimi02@gmail.com>
2023-02-03 10:24:02 +0100

    Converted from Js to Tsx for React

--- ProfileMatching/wwwroot/Views/frontend/src/App.tsx
+++ ProfileMatching/wwwroot/Views/frontend/src/App.tsx
@@ -4,14 +4,12 @@ import JobPosition from './JobPosition';
 import SearchAppBar from './SearchAppBar';
 import { Box } from '@mui/system';
 
-
 function App() {
   return (
     <div className="App">
       <SearchAppBar></SearchAppBar>
       <Box mt={13}></Box>
       <JobPosition></JobPosition>
-      
     </div>
   );
 }
