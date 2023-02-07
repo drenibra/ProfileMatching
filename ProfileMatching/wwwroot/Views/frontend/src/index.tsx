3218244080916ccae6b19ae37041ea7492ec43d6
drenibra <drenibrahimi02@gmail.com>
2023-02-07 16:36:03 +0100

    Fixed the problem with Typescript

--- ProfileMatching/wwwroot/Views/frontend/src/index.tsx
+++ ProfileMatching/wwwroot/Views/frontend/src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/layout/App';
-import { store, StoreContext } from './app/stores/store';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
